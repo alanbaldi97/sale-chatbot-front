1619790fcfaf370bbc261a373bab2ce8cd14c86c
Alan Ortiz <76020153+alanbaldi97@users.noreply.github.com>
2025-10-15 16:47:46 -0500

    chore(app.config.ts): improvement modal

--- app/app.config.ts
+++ app/app.config.ts
@@ -13,6 +13,11 @@ export default defineAppConfig({
         kbds: "hidden lg:inline-flex items-center shrink-0 gap-0.5 not-first-of-type:before:content-['·'] not-first-of-type:before:me-0.5",
         kbdsSize: 'sm'
       }
+    },
+    modal: {
+      slots: {
+        overlay: 'backdrop-blur-xs bg-black/5 dark:bg-black/50',
+      }
     }
   }
 })
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -14,5 +14,5 @@ export default defineNuxtConfig({
   },
   runtimeConfig: {
     apiBase: process.env.NUXT_PUBLIC_API_BASE_URL || 'http://localhost:3000',
-  },
+  }
 })
